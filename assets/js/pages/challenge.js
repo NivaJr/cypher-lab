9c3eafdd454c2e17d220165bad61d31ef2b35587
LFMineiro <lflmalbuquerque3@gmail.com>
2025-10-26 23:31:20 -0300

    feat: return feedback to user after submitting answer (closes #1)

--- assets/js/pages/challenge.js
+++ assets/js/pages/challenge.js
@@ -3,14 +3,15 @@
     const form = document.getElementById('submitForm');
     const input = document.getElementById('submitInput');
     const button = document.getElementById('submitButton');
+    const feedback = document.getElementById('submitFeedback');
 
     form.addEventListener('submit', async (e) => {
         
         e.preventDefault();
         const userAnswer = input.value.trim()
-        console.log(userAnswer)
+        const challengeId = 3
         
-        const res = await fetch(`${API_URL}/challenges/${challengeId}/answer`, {
+        const res = await fetch(`${API_URL}/challenges/${challengeId}/submit`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -19,6 +20,10 @@
                 ans: userAnswer
             })
         } )
+        
+        const data = await res.json();
+        feedback.innerText = data.message;
+        feedback.style.color = data.correct ? 'green' : 'red';
         
             
     })
