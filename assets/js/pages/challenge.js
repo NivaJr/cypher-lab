3f935e296464d0db4f1434dc52471401d70edc88
LFMineiro <lflmalbuquerque3@gmail.com>
2025-10-28 10:37:50 -0300

    feat: render challenge data on frontend (closes #2)

--- assets/js/pages/challenge.js
+++ assets/js/pages/challenge.js
@@ -1,15 +1,36 @@
-    API_URL = 'http://localhost:8080/api';
-
+    import { fetchChallenge } from "../services/challengeService.js"
+    
+    const API_URL = 'http://localhost:8080/api';
+    
+    const challengeId = 3
+    
     const form = document.getElementById('submitForm');
     const input = document.getElementById('submitInput');
     const button = document.getElementById('submitButton');
     const feedback = document.getElementById('submitFeedback');
 
+    const challengeTitle = document.getElementById('challengeTitle');
+    const challengeCategory = document.getElementById('challengeCategory');
+    const instructions = document.getElementById('instructions');
+    const difficulty = document.getElementById('difficulty');
+    const category = document.getElementById('category');
+    const reward = document.getElementById('reward');
+
+    const challengeData = await fetchChallenge(challengeId);
+
+    challengeTitle.innerText = `Desafio: ${challengeData.title}`;
+    challengeCategory.innerText = challengeData.category
+    category.innerText = challengeData.category
+    instructions.innerText = challengeData.description
+    difficulty.innerText = challengeData.difficulty
+   
+
+
+
     form.addEventListener('submit', async (e) => {
         
         e.preventDefault();
         const userAnswer = input.value.trim()
-        const challengeId = 3
         
         const res = await fetch(`${API_URL}/challenges/${challengeId}/submit`, {
             method: 'POST',
@@ -21,12 +42,13 @@
             })
         } )
         
+        
         const data = await res.json();
         feedback.innerText = data.message;
         feedback.style.color = data.correct ? 'green' : 'red';
         
-            
-    })
+    });
+
 
 
 
