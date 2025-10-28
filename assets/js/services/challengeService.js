d7827fd88862b877601f558ac3a85cd9e0fea656
LFMineiro <lflmalbuquerque3@gmail.com>
2025-10-28 10:32:59 -0300

    feat: fetch challenge details by id using Fetch API

--- assets/js/services/challengeService.js
+++ assets/js/services/challengeService.js
@@ -1,10 +1,10 @@
-import { API_URL } from "./api"
-
+const API_URL = 'http://localhost:8080/api';
 
 export async function fetchChallenge(challengeId) {
-    const res = await fetch(`${API_URL}/challenges/${challengeId}`)
+    const url = API_URL + `/challenges/${challengeId}`;
+    const res = await fetch(url)
     if (!res.ok) {
-        throw new Error('Erro ao buscar desafio')
+        throw new Error('Erro ao buscar desafio', res.status);
     }
     return res.json()
 }
