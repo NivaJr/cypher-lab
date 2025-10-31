7d46bc572f3d1d7c8f48e213dad9b6a57d775bbe
LFMineiro <lflmalbuquerque3@gmail.com>
2025-10-31 18:13:04 -0300

    feat: render challenge only after fetch, show not found page immediately if challenge does not exist (#6)

--- assets/js/pages/challenge.js
+++ assets/js/pages/challenge.js
@@ -8,7 +8,8 @@
     const input = document.getElementById('submitInput');
     const button = document.getElementById('submitButton');
     const feedback = document.getElementById('submitFeedback');
-
+    
+    const challengeContent = document.querySelector('.challenge-content');
     const challengeTitle = document.getElementById('challengeTitle');
     const challengeCategory = document.getElementById('challengeCategory');
     const instructions = document.getElementById('instructions');
@@ -19,39 +20,66 @@
 
 
 
-    try {
-        const challengeData = await fetchChallenge(challengeId)
+  
+        fetchChallenge(challengeId)
+        .then(challengeData => {
+            challengeContent.innerHTML = `
+            <div class="challenge-title">
+                <h1 id="challengeTitle">Desafio: ${challengeData.title}</h1>
+                <p id="challengeCategory">${challengeData.category}</p>
+            </div>
+    
+            <div class="challenge-box challenge-description">
+                <p class="description-title">Instruções</p>
+                <p class="instructions" id="instructions"> ${challengeData.description}
+                </p>
+                <div class="challenge-details" id="challenge-details">
+                    <p class="details" id="difficulty">${challengeData.difficulty}</p>
+                    <p class="details" id="category">${challengeData.category}</p>
+                    <p class="details" id="reward">${challengeData.reward} pontos</p>
+                </div>
+            </div>
+    
+            <form class="challenge-box challenge-submit" id="submitForm">
+                <h2>Digite sua resposta:</h2>
+                <div class="challenge-submit-input">
+                    <input type="text" id="submitInput" placeholder="Digite o texto cifrado aqui...">
+                    <button type="submit" id="submitButton">Verificar resposta</button>
+                </div>
+                <div class="feedback" id="submitFeedback"></div>
+            
+            `
+        })
+        .catch( error => {
+                challengeContent.innerHTML = `
+                    <div class="not-found-content">
+                        <h1 class="not-found-title">
+                            Desafio não encontrado
+                        </h1>
+                        <p class="not-found-text">
+                            O desafio que você procura não existe
+                        </p>
+                        <button class="btn">
+                            Retornar para o dashboard
+                        </button>
+                    </div>
+                `;
+       } ) 
     
     
-        challengeTitle.innerText = `Desafio: ${challengeData.title}`;
+        
+        
+        
+        {/* challengeTitle.innerText = `Desafio: ${challengeData.title}`;
         challengeCategory.innerText = challengeData.category
         category.innerText = challengeData.category
         instructions.innerText = challengeData.description
         difficulty.innerText = challengeData.difficulty
-        reward.innerText = `${challengeData.reward} pontos`
+        reward.innerText = `${challengeData.reward} pontos` */}
 
         
        
 
-    } catch {
-        const challengeContent = document.querySelector('.challenge-content');
-        challengeContent.innerHTML = `
-            <div class="not-found-content">
-                <h1 class="not-found-title">
-                    Desafio não encontrado
-                </h1>
-                <p class="not-found-text">
-                    O desafio que você procura não existe
-                </p>
-                <button class="btn">
-                    Retornar para o dashboard
-                </button>
-            </div>
-        `;
-
-
-    
-    }
 
 
 
