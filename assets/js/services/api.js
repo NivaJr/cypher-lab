724c738605b797769ec764adc085a94568936aa1
Álisson Santana <45105953+alisson94@users.noreply.github.com>
2025-11-22 15:38:27 -0300

    Remove social login and related code from auth pages

--- assets/js/services/api.js
+++ assets/js/services/api.js
@@ -2,7 +2,6 @@ const BACKEND_HOST = 'localhost';
 const BACKEND_PORT = '8081';
 export const API_URL = `http://${BACKEND_HOST}:${BACKEND_PORT}/api`;
 export const AUTH_URL = `http://${BACKEND_HOST}:${BACKEND_PORT}/auth`;
-export const OAUTH2_URL = `http://${BACKEND_HOST}:${BACKEND_PORT}/oauth2`;
 
 export const getToken = ()=> {
     return localStorage.getItem("authToken");
